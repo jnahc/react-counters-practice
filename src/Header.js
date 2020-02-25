dc9ca93399338589a9c7bbac122ffd6367ff5993
jeff-chan-sf-sei-05 <emailjeffchan@gmail.com>
2020-02-24 21:26:43 -0800

    some ink styling

--- src/Header.js
+++ src/Header.js
@@ -2,10 +2,14 @@ import React from 'react';
 
 const Header = props => {
   return (
-    <header className="App-header">
-      <h1 className="App-title">React Counters</h1>
-      <button onClick={props.increaseCounters}>+</button>
-      <button onClick={props.decreaseCounters}>-</button>
+    <header className="ink-navigation">     
+      <h1 className="slab-300">React Counters</h1>
+      <div className="button-toolbar">
+        <div className="button-group">
+          <button className="ink-button" onClick={props.increaseCounters}>Add Counter</button>
+          <button className="ink-button" onClick={props.decreaseCounters}>Remove Counter</button>
+        </div>
+      </div>
     </header>
   );
 }
